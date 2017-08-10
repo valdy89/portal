bfe81ec5ab99e8ccc8990cdec988ac92aec59917
valdy <valusekm@gmail.com>
2017-08-10 18:17:57 +0200

    subtenants - first attempt

--- portal/src/main/frontend/src/app/subtenants/subtenants.js
+++ portal/src/main/frontend/src/app/subtenants/subtenants.js
@@ -1,28 +1,74 @@
 /*globals alert */
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  angular
-    .module('portal')
-    .controller('SubtenantsController', SubtenantsController);
+    angular
+      .module('portal')
+      .controller('SubtenantsController', SubtenantsController)
+      .controller('CreateSubtenantController', SubtenantsController);
 
-  /** @ngInject */
-  function SubtenantsController($log, $rootScope, TenantResource, $mdDialog, $mdToast) {
-    var ctrl = this;
-    $log.debug("user data: " + $rootScope.userData);
-    ctrl.userData = $rootScope.userData;
+    /** @ngInject */
+    function SubtenantsController($log, $rootScope, TenantResource, $mdDialog) {
+      var ctrl = this;
+      $log.debug("user data: " + $rootScope.userData);
+      ctrl.userData = $rootScope.userData;
 
-    if (ctrl.userData) {
-      ctrl.tenant = TenantResource.get();
-      console.log(ctrl.tenant);
-      ctrl.tenant.$promise.then(
-        function () {
+      if (ctrl.userData) {
+        ctrl.tenant = TenantResource.get();
+        ctrl.tenant.$promise.then(
+          function() {
 
-          ctrl.repositoryData = [(pom / 1024).toFixed(1), (ctrl.tenant.usedQuota / 1024).toFixed(1)];
-        },
-        function (error) {
-          alert(error.data.message);
+            ctrl.repositoryData = [(pom / 1024).toFixed(1), (ctrl.tenant.usedQuota / 1024).toFixed(1)];
+          },
+          function(error) {
+            alert(error.data.message);
+          });
+      }
+
+      ctrl.createSubtenant = function() {
+        var modalInstance = $mdDialog.show({
+          //animation: false,
+          templateUrl: 'createSubtenant.html',
+          controller: 'CreateSubtenantController as ctrl',
+          parent: angular.element(document.body),
+          clickOutsideToClose: true,
+          fullscreen: $rootScope.customFullscreen,
+          locals: {
+            tenant: ctrl.tenant
+          }
         });
-  }
-}
-})();
+
+        modalInstance.finally(function() {
+          ctrl.tenant = TenantResource.get();
+        });
+      };
+
+
+
+      }
+
+
+      function CreateSubtenantController($log, $mdDialog, $http, tenant, EndpointConfigService) {
+        var ctrl = this;
+
+        ctrl.tenant = tenant;
+
+        ctrl.subtenant.quota = ctrl.tenant.quota / 1024;
+
+        ctrl.save = function() {
+          //todo: call backend
+          // ctrl.promise = $http.post(EndpointConfigService.getUrl('/tenant'), {quota: ctrl.quota * 1024});
+          // ctrl.promise.then(function (response) {
+          //   $mdDialog.hide();
+          // }, function (response) {
+          //   alert(response.data.message);
+          // });
+
+        };
+
+        ctrl.cancel = function() {
+          $mdDialog.hide('cancel');
+        };
+      }
+
+    })();
